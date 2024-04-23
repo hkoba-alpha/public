f3fd960f46b055ac3e48aeda47bb56bd89b596dd
Hirokazu Kobayashi <hkoba.alpha@me.com>
2024-04-23 20:14:55 +0900

    テトリス追加

---
+++ docs/tetris/precache-manifest.8d1e7100d3065b7c23658d8b246448b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b1e1a45c2704775879993b0efb5fb7",
+    "url": "/public/tetris/index.html"
+  },
+  {
+    "revision": "1ae1ba4acf7df108ca60",
+    "url": "/public/tetris/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "828d6723c5f67f5ad77e",
+    "url": "/public/tetris/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1ae1ba4acf7df108ca60",
+    "url": "/public/tetris/static/js/2.7354caa5.chunk.js"
+  },
+  {
+    "revision": "98ba7388b8e6f4cfa8d1",
+    "url": "/public/tetris/static/js/3.34b8d668.chunk.js"
+  },
+  {
+    "revision": "828d6723c5f67f5ad77e",
+    "url": "/public/tetris/static/js/main.eb1ef525.chunk.js"
+  },
+  {
+    "revision": "9890a91f7d775b150dc9",
+    "url": "/public/tetris/static/js/runtime~main.ec90b5a1.js"
+  }
+]);
---
+++ docs/tetris/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/public/tetris/precache-manifest.8d1e7100d3065b7c23658d8b246448b4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/public/tetris/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
